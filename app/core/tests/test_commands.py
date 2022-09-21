139f9f2dcfa49f1bc0f18f2cb8e54cad7d8662bb
Ginny-Yang <ginny.yang@slimmer.ai>
2022-09-22 00:24:00 +0200

    fix:test_command.py

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,7 +3,7 @@ Test custom Django management commands. vid_39
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -12,7 +12,7 @@ from django.test import SimpleTestCase
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
-    """TEst commands."""
+    """Test commands."""
 
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
@@ -25,10 +25,10 @@ class CommandTests(SimpleTestCase):
     @patch('time.sleep')
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError."""
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
+        patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
 
         call_command('wait_for_db')
 
         self.assertEqual(patched_check.call_count, 6)
-        patched_check.assert_called_once_with(databases=['default'])
+        patched_check.assert_called_with(databases=['default'])
