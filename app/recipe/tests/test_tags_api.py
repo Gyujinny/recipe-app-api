bace4055f84163efb04ca1ed9ef0caa62f7c7f53
Ginny-Yang <ginny.yang@slimmer.ai>
2022-10-25 14:18:47 +0200

    WIP2 vid 92

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -5,7 +5,6 @@ from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.test import TestCase
 
-
 from rest_framework import status
 from rest_framework.test import APIClient
 
@@ -13,7 +12,9 @@ from core.models import Tag
 
 from recipe.serializers import TagSerializer
 
+
 TAGS_URL = reverse('recipe:tag-list')
+
 
 def create_user(email='user@example.com', password='testpass123'):
     """Create and return a user."""
@@ -23,6 +24,9 @@ class PublicTagsApiTests(TestCase):
     """Test unauthenticated API requests."""
 
     def setUp(self):
+        self.client = APIClient
+
+    def test_auth_required(self):
         """Test auth is required for retrieving tags."""
         res = self.client.get(TAGS_URL)
 
@@ -31,6 +35,7 @@ class PublicTagsApiTests(TestCase):
 
 class PrivateTagsApiTests(TestCase):
     """Test authenticated API requests."""
+
     def setUp(self):
         self.user = create_user()
         self.client = APIClient()
@@ -44,15 +49,15 @@ class PrivateTagsApiTests(TestCase):
         res = self.client.get(TAGS_URL)
 
         tags = Tag.objects.all().order_by('-name')
-        serializers = TagSerializer(tags, many=True)
+        serializer = TagSerializer(tags, many=True)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data, serializers.data)
+        self.assertEqual(res.data, serializer.data)
 
     def test_tags_limited_to_user(self):
         """Test list of tags is limited to authenticated user."""
         user2 = create_user(email='user2@example.com')
-        Tag.objects.create(user=self.user, name="Fruity")
-        tags = Tag.objects.create(user=self.user, name='Comfort Food')
+        Tag.objects.create(user=user2, name='Fruity')
+        tag = Tag.objects.create(user=self.user, name='Comfort Food')
 
         res = self.client.get(TAGS_URL)
 
